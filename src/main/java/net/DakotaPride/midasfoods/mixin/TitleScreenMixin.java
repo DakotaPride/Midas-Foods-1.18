8d8ac46dd7c8c661007d886ed98007d4a617c4a3
DakotaPride <rjtube13@gmail.com>
2022-02-11 09:56:57 -0600

    Midas Foods

--- src/main/java/net/DakotaPride/midasfoods/mixin/TitleScreenMixin.java
+++ src/main/java/net/DakotaPride/midasfoods/mixin/TitleScreenMixin.java
@@ -3,14 +3,23 @@ package net.DakotaPride.midasfoods.mixin;
 
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.lang.invoke.LambdaMetafactory;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodType;
+
 @Mixin(TitleScreen.class)
 public abstract class TitleScreenMixin extends Screen {
+
+    private static final Object V = null;
+    private Object ButtonWidget;
 
     public TitleScreenMixin(Text title) {
         super(title);
