329e6168026dac3e9e5da523fd3879a7a2980154
DakotaPride <rjtube13@gmail.com>
2022-02-16 10:55:20 -0600

    Midas Foods

--- src/main/java/net/DakotaPride/midasfoods/mixin/TitleScreenMixin.java
+++ src/main/java/net/DakotaPride/midasfoods/mixin/TitleScreenMixin.java
@@ -4,30 +4,27 @@ package net.DakotaPride.midasfoods.mixin;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Util;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.lang.invoke.LambdaMetafactory;
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodType;
-
 @Mixin(TitleScreen.class)
 public abstract class TitleScreenMixin extends Screen {
-
-    private static final Object V = null;
-    private Object ButtonWidget;
 
     public TitleScreenMixin(Text title) {
         super(title);
     }
 
     @Inject(at = @At("RETURN"), method = "initWidgetsNormal")
-    private void MoreWeaponryButton(int y, int spacingY, CallbackInfo info) {
-
+        private void customButtonMod(int y, int spacingY, CallbackInfo ci) {
+        this.addDrawableChild(new ButtonWidget(this.width /
+                2 + 385, y + -190 + 12, 90, 20, new LiteralText("More Weaponry"), (button) -> {
+            Util.getOperatingSystem().open("https://www.curseforge.com/minecraft/mc-mods/more-weaponry");;
+        }));
     }
 
 }
